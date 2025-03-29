dcbe264b799ed708145afdb3c6d8824e85c8f472
Al Kaosar Hussain <alkaoser4@gmail.com>
2025-03-29 20:23:55 +0600

    Add files via upload

---
+++ Chinese Remainder Theorem.cpp
@@ -0,0 +1,11 @@
+typedef long long ll;
+
+ll crt(ll a, ll m, ll b, ll n)
+{
+    if (n > m)
+        swap(a, b), swap(m, n);
+    ll x, y, g = euclid(m, n, x, y);
+    assert((a - b) % g == 0);
+    x = (b - a) % n * x % n / g * m + a;
+    return x < 0 ? x + m * n / g : x;
+}
