6c4963801180c0cc21085f9b5b694b050ff24167
Al Kaosar Hussain <alkaoser4@gmail.com>
2025-07-09 19:53:41 +0600

    Add files via upload

---
+++ Linear Transformation.cpp
@@ -0,0 +1,38 @@
+template <class T>
+int sgn(T x)
+{
+	return (x > 0) - (x < 0);
+}
+template <class T>
+struct Point
+{
+	typedef Point P;
+	T x, y;
+	explicit Point(T x = 0, T y = 0) : x(x), y(y) {}
+	bool operator<(P p) const { return tie(x, y) < tie(p.x, p.y); }
+	bool operator==(P p) const { return tie(x, y) == tie(p.x, p.y); }
+	P operator+(P p) const { return P(x + p.x, y + p.y); }
+	P operator-(P p) const { return P(x - p.x, y - p.y); }
+	P operator*(T d) const { return P(x * d, y * d); }
+	P operator/(T d) const { return P(x / d, y / d); }
+	T dot(P p) const { return x * p.x + y * p.y; }
+	T cross(P p) const { return x * p.y - y * p.x; }
+	T cross(P a, P b) const { return (a - *this).cross(b - *this); }
+	T dist2() const { return x * x + y * y; }
+	double dist() const { return sqrt((double)dist2()); }
+	double angle() const { return atan2(y, x); }
+	P unit() const { return *this / dist(); }
+	P perp() const { return P(-y, x); }
+	P normal() const { return perp().unit(); }
+	P rotate(double a) const { return P(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a)); }
+	friend ostream &operator<<(ostream &os, P p) { return os << "(" << p.x << "," << p.y << ")"; }
+};
+typedef Point<double> P;
+
+typedef Point<double> P;
+P linearTransformation(const P &p0, const P &p1,
+							  const P &q0, const P &q1, const P &r)
+{
+	P dp = p1 - p0, dq = q1 - q0, num(dp.cross(dq), dp.dot(dq));
+	return q0 + P((r - p0).cross(num), (r - p0).dot(num)) / dp.dist2();
+}
